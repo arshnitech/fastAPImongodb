572a1705f1408ab0b158dd451fc37515c2ee9f54
arshnitech <124500205+arshnitech@users.noreply.github.com>
2026-02-19 14:24:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 # 🔐 MongoDB Connection
-MONGO_URL = "mongodb+srv://azureuser:aA1234567890@swarnamongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+MONGO_URL = "
+mongodb+srv://azureuser:aA1234567890@swarnamongodb1.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.ecommerceDB
